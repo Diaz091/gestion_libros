5116da3c321b6bd6c599039e28ecf35eb190bbb4
Unai <unai_01012@hotmail.com>
2018-10-05 23:50:32 +0200

    Edición Border class

--- src/resources/Border.java
+++ src/resources/Border.java
@@ -1,18 +1,24 @@
 package resources;
 
+import java.awt.Color;
+
 import javax.swing.BorderFactory;
-import javax.swing.border.EtchedBorder;
 
 public class Border {
+  
 
-	public  Border() {
-		
-		Border[] border = new Border[] { (Border) BorderFactory.createEtchedBorder(EtchedBorder.LOWERED)
-				
-				
-				
-		};
-	}
-	
+	public Border() {
+       Border[] border = { BorderFactory.createEtchedBorder(),
+			    BorderFactory.createTitledBorder("Border types"),
+			    BorderFactory.createLoweredBevelBorder(),
+			    BorderFactory.createRaisedBevelBorder(),
+			    BorderFactory.createEtchedBorder(),
+			    BorderFactory.createLineBorder(Color.blue),
+			    BorderFactory.createMatteBorder(10, 10, 10, 10, Color.blue),
+			    BorderFactory.createEmptyBorder()
+       };
+ 
+  }
 
+  
 }
